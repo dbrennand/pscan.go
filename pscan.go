5aec22cee64bf09e6fb8bc61f29663efe581c579
Dextroz <dextrozmlg@gmail.com>
2018-06-04 12:24:14 +0100

    Fixed issue with .Dial presenting err.

--- pscan.go
+++ pscan.go
@@ -17,23 +17,28 @@ func addr_port_input() string {
 	reader := bufio.NewReader(os.Stdin)
 	fmt.Print("Example: www.hackthissite.org:80\n")
 	fmt.Print("Server Address & Port: ")
-	addr_port, err := reader.ReadString('\n')
+	input, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Println("Failed to read string: ", err)
 	}
+	// Done to remove the '\n' from str input. Otherwise, net.Dial will fail.
+	addr_port := strings.Replace(input, "\n", "", -1)
 	return addr_port
 }
 
 func connect(addr_port string) {
-	// I believe the issue is with .Dial() or im doing something obviously wrong
-	// and just cant see it.
+	// addr_port is passed into .Dial function.
 	conn, err := net.Dial("tcp", addr_port)
+	// Split str at ':' to get the specific port number.
 	port := strings.Split(addr_port, ":")
+	// If Err tell user connection failed and presented err.
 	if err != nil {
-		fmt.Println("Error occured when connecting:", err)
-		fmt.Printf("Port %v is closed", port[1])
-	} else {
-		fmt.Printf("Port %v is open", port[1])
+		fmt.Printf("Connection Unsuccessful - Port %v is closed\n", port[1])
+		fmt.Println("Error: ", err)
+		// else if the connection is successful, tell user.
+	} else if conn != nil {
+		fmt.Printf("Connection Successful - Port %v is open\n", port[1])
 	}
+	// Close the connection.
 	conn.Close()
 }
